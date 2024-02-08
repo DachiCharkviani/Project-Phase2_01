d40a3953e0e00c354d283131523341c5b1262fc6
Davit Charkviani <dcharkvi@andrew.cmu.edu>
2024-02-08 09:34:00 +0300

    made changes to the book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,9 @@
 class Book < ApplicationRecord
-  belongs_to :publisher_id
+  belongs_to :publisher
+  has_many :book_authors
+  has_many :authors, through: :book_authors
+
+  '''ruby
+  # scope that returns books in alphabetical order
+  '''
 end
